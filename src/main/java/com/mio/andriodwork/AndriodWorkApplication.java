4ea77b63d978481ad84315316b92481151f00be7
MIO-0701 <1446180775@qq.com>
2026-05-11 09:49:18 +0800

    https://github.com/MIO-0701/ApplicationWork.git

---
+++ src/main/java/com/mio/andriodwork/AndriodWorkApplication.java
@@ -0,0 +1,13 @@
+package com.mio.andriodwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndriodWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndriodWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mio/andriodwork/AndriodWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mio.andriodwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndriodWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
